86364df2f457f7b693761ca7507ff58c5b462caa
Joris Calvat <jcalvat@qxlricardo.internal>
2017-02-07 10:23:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pdp-push-desktop
+
+
+
---
+++ src/pdp-push-desktop.js
@@ -0,0 +1,71 @@
+const isApiAvailable = () => {
+
+    return "Notification" in window;
+}
+
+const isPermissionGranted = () => {
+    return Notification.permission === "granted";
+}
+
+const hasPermission = () => {
+
+    return 'permission' in Notification;
+}
+
+const setPermission = (permission) => {
+    Notification.permission = permission;
+}
+
+const sendNotification = (message, options = {}) => {
+    return new Notification(message, options);
+}
+
+const requestPermission = () => {
+
+    return new Promise((resolve, reject) => {
+
+        Notification.requestPermission((permission) => {
+
+            resolve (permission);
+        });
+    });
+};
+
+
+
+const pdpPushDesktop = {
+
+    push(message, options) {
+        return new Promise((resolve, reject) => {
+
+            if (!isApiAvailable()) {
+
+                reject('Notification not supported by the browser');
+                return;
+            }
+            else if (isPermissionGranted()) {
+                
+                resolve(sendNotification(message, options));
+            }
+            else if(!hasPermission()) {
+                
+                requestPermission()
+                .then((permission) => {
+                    setPermission(permission);
+                    if(isPermissionGranted()) {
+                        resolve(sendNotification(message, options));
+                    }
+                    else {
+                        reject('Notification not allowed');
+                    }
+                })
+            }
+        });
+
+
+    }
+};
+
+window.pdpPushDesktop = pdpPushDesktop;
+
+export default pdpPushDesktop;
